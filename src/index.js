3c8abec1ac298db34e1c1abf8d9dc101109b72b5
ashwinRP7 <ashwin.patil@launchventures.co>
2020-08-19 16:50:08 +0530

    warmup2CompletedwithCSS

--- src/index.js
+++ src/index.js
@@ -4,9 +4,57 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function Imagebox(props){
+  return(
+    <div className="imageBox">
+      {props.imageDetails}
+    </div>
+  );
+}
+
+function TitleBox(props){
+  return(
+    <div className="titleBox">
+      {props.titleDetails}
+    </div>
+  );
+}
+
+function DescBox(props){
+  return(
+    <div className="descBox">
+      {props.descDetails}
+    </div>
+  );
+}
+
+function RightBox(props){
+  return(
+    <div className="rightBox">
+      <TitleBox titleDetails = {props.rightBoxDetails.titleContent}></TitleBox>
+      <DescBox descDetails = {props.rightBoxDetails.descContent}></DescBox>
+    </div>
+  );
+}
+
+function OuterBox(props){
+  return(
+    <div className="outerBox">
+      <Imagebox imageDetails = {props.details.imageContent}></Imagebox>
+      <RightBox rightBoxDetails = {props.details}></RightBox>
+    </div>
+  );
+}
+
+const content = {
+  titleContent:"A Title",
+  imageContent:"IMG",
+  descContent:"The description goes here.",
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <OuterBox details = {content}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
